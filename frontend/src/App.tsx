338bc77a746c61f2861ee1ab7bd791da9a875990
Patrik Pyrzanowski <p.pyrzano.s6@gmail.com>
2022-07-21 13:52:51 +0200

    Fix fetch content-type header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,6 +19,9 @@ function App() {
 
     fetch("/api/save", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(exercise),
     })
   }
